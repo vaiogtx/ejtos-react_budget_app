c69ee8b046f2bd762a2c89a13aef230f46a00de3
Angus <131868779+vaiogtx@users.noreply.github.com>
2023-06-12 15:27:29 +0700

    Add Budget component

--- src/App.js
+++ src/App.js
@@ -15,17 +15,13 @@ const App = () => {
             <div className='container'>
                 <h1 className='mt-3'>Company's Budget Allocation</h1>
                     <div className='row mt-3'>
-                        {/* Add Budget component here under */}        
-
-                        {/* Add Remaining component here under */}        
-
-                        {/* Add ExpenseTotal component here under */}        
-                       
-                        {/* Add ExpenseList component here under */}         
-
-                        {/* Add ExpenseItem component here under */}        
-
-                        {/* Add AllocationForm component here under */}        
+                        {/* Add Budget component here under */}
+                        <div className="col-sm"><Budget></Budget></div>
+                        {/* Add Remaining component here under */}
+                        {/* Add ExpenseTotal component here under */}
+                        {/* Add ExpenseList component here under */}
+                        {/* Add ExpenseItem component here under */}
+                        {/* Add AllocationForm component here under */}
 
                 </div>
             </div>
